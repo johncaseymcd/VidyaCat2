02797e7f28fccc42ccd029a98751c1eca2c5e1a4
John Casey McDonough <johncaseymcd@gmail.com>
2021-11-15 17:18:08 -0500

    Got it working again I think?

---
+++ VidyaCat2.Data/Migrations/202111102305325_WorkDammit.cs
@@ -0,0 +1,16 @@
+﻿namespace VidyaCat2.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class WorkDammit : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
